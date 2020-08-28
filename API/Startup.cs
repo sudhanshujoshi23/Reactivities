8612b9b3535efabea5a5eb725f55822849946cd1
Sudhanshu Joshi1 <sudhanshu.joshi1@synapse.com>
2020-08-28 23:58:35 +0530

    End of Section-3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,12 @@ namespace API
             }
 
             );
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy", policy =>{
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
 
         }
@@ -49,6 +55,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
